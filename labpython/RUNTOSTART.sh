4dfdc816f20be1e9da8394ef8d522759d6180e30
David Low: Manchot <dhl88@cornell.edu>
2017-07-03 16:49:45 -0400

    Added labpython, python for lab code.
    From gmf57's python docker file

---
+++ labpython/RUNTOSTART.sh
@@ -0,0 +1,7 @@
+docker run --rm -it --privileged \
+    -e DISPLAY=$DISPLAY  \ 
+    -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0  \
+    -v /mnt/labshare:/labshare 
+    -v /home/david/cornell/nowack_lab/labnotebook:/labnotebook \
+    -v /home/david/cornell/nowack_lab/code:/code  \
+    gmf57/python
