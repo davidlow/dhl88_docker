7ce70604efe1d5ae475578e83cbf39ca84d800cf
David Low: Manchot <dhl88@cornell.edu>
2017-07-06 16:18:50 -0400

    Fixed labpython, added a make file and a run file

--- labpython/run.sh
+++ labpython/run.sh
@@ -1,7 +1,8 @@
 docker run --rm -it --privileged \
     -e DISPLAY=$DISPLAY  \
+    -e PYTHONPATH="/code:/labnotebook" \
     -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0  \
     -v /mnt/labshare:/labshare \
     -v /home/david/cornell/nowack_lab/labnotebook:/labnotebook \
     -v /home/david/cornell/nowack_lab/code:/code  \
-    gmf57/python
+    dhl88/labpython
