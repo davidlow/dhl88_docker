87ef1619477b2fcdf08480d9262db0bffd56facd
David Low: Manchot <dhl88@cornell.edu>
2017-08-11 19:35:08 -0400

    Fixed sound on normal firefox, added firefox_arch that works as well
    Added firefox_arch to see if the problem with sound was from the
    different distros.  It turned the problem was that /run/user/1000
    was not created so pulse could not find the right place to export
    the sound

--- firefox_arch/run.sh
+++ firefox_arch/run.sh
@@ -9,11 +9,11 @@ docker run \
     -v /mnt/labshare:/labshare \
     --device /dev/snd \
     -v /dev/shm:/dev/shm \
-    -v /run/user/1000/pulse:/run/user/1000/pulse \
     -v ~/.pulse:/root/.pulse \
+    -v /run/user/1000/pulse:/run/user/1000/pulse \
     -v /etc/machine-id:/etc/machine-id \
     -v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket \
-    --name firefox \
-    dhl88/firefox
+    --name firefox_arch \
+    dhl88/firefox_arch
 
 #    -v /dev/snd:/dev/snd \ # for alsa audio
