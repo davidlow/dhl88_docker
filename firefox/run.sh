590aadf826b0cf2da7308ebf3b1430089ddbc280
David Low (gb1) <dhl88@cornell.edu>
2017-09-03 18:36:52 -0400

    Fixed docker firefox on gb1

--- firefox/run.sh
+++ firefox/run.sh
@@ -4,12 +4,12 @@ docker run \
     --privileged \
     -e DISPLAY=$DISPLAY  \
     -e uid=$(id -u) \
-    -e gid=$(id -g) \
+    -e gid=996 \
     -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0  \
     -v /mnt/labshare:/labshare \
     --device /dev/snd \
     -v /dev/shm:/dev/shm \
-    -v /run/user/1000/pulse:/run/user/1000/pulse \
+    -v /run/user/1001/pulse:/run/user/1001/pulse \
     -v ~/.pulse:/root/.pulse \
     -v /etc/machine-id:/etc/machine-id \
     -v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket \
