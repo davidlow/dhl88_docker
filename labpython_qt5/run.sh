46137663bb6e4260892c17b218990bdf5dd1f643
David Low: Manchot <dhl88@cornell.edu>
2017-10-18 19:28:48 -0400

    added labpython with pyqt5 and pyqtgraph

---
+++ labpython_qt5/run.sh
@@ -0,0 +1,12 @@
+docker run \
+    --rm  \
+    -it \
+    --privileged \
+    -e DISPLAY=$DISPLAY  \
+    -e PYTHONPATH="/code:/labnotebook" \
+    -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0  \
+    -v /mnt/labshare:/labshare \
+    -v /home/david/cornell/nowack_lab/labnotebook:/labnotebook \
+    -v /home/david/cornell/nowack_lab/code:/code  \
+    --name labpython_qt \
+    dhl88/labpython_qt
