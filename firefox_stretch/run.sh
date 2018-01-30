8e06480621893207776c7e089ff66db4cf45829c
david <david@damddesk.home>
2018-01-29 20:28:58 -0500

    Firefox on debian stretch.  Not a recent version
    Do not use

---
+++ firefox_stretch/run.sh
@@ -0,0 +1,19 @@
+docker run \
+    --rm  \
+    -it  \
+    --privileged \
+    -e DISPLAY=$DISPLAY  \
+    -e uid=$(id -u) \
+    -e gid=996 \
+    -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0  \
+    -v /home/david/Downloads/docker:/downloads \
+    --device /dev/snd \
+    -v /dev/shm:/dev/shm \
+    -v /run/user/1001/pulse:/run/user/1001/pulse \
+    -v ~/.pulse:/root/.pulse \
+    -v /etc/machine-id:/etc/machine-id \
+    -v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket \
+    --name firefox \
+    dhl88/firefox
+
+#    -v /dev/snd:/dev/snd \ # for alsa audio
