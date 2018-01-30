47b4b6f0517ebb575bc87cb2fb0a38bd9544451d
david <david@damddesk.home>
2018-01-29 20:30:32 -0500

    mplayer on stretch
    Video

---
+++ mplayer_stretch/run.sh
@@ -0,0 +1,17 @@
+docker run \
+    --rm  \
+    -it  \
+    --privileged \
+    -e DISPLAY=$DISPLAY  \
+    -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0  \
+    -v /home/david/Downloads/docker:/downloads \
+    --device /dev/snd \
+    -v /dev/shm:/dev/shm \
+    -v /run/user/$(id -u)/pulse:/run/user/$(id -u)/pulse \
+    -v ~/.pulse:/root/.pulse \
+    -v /etc/machine-id:/etc/machine-id \
+    -v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket \
+    --name mplayer-stretch \
+    dhl88/mplayer-stretch
+
+#    -v /dev/snd:/dev/snd \ # for alsa audio
