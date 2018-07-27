810eaca199adfe6557b47b3423e80b17676e1c36
David Low: Manchot <dhl88@cornell.edu>
2018-07-27 17:05:42 -0400

    Dark colorstyle in matplotlib

--- labpython/run.sh
+++ labpython/run.sh
@@ -6,7 +6,7 @@ docker run \
     -e PYTHONPATH="/code:/labnotebook" \
     -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0  \
     -v /mnt/labshare:/labshare \
-    -v /home/david/cornell/labnotebook:/labnotebook \
+    -v /home/david/cornell/nowack_lab/labnotebook:/labnotebook \
     -v /home/david/cornell/nowack_lab/code:/code  \
-    --name labpython2 \
+    --name labpython \
     dhl88/labpython
