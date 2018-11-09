8b141c2b12761de80e58588eacdcf0aefb17779f
David Low: Manchot <dhl88@cornell.edu>
2018-11-09 13:16:03 -0500

    hdfview access to labnotebook

--- hdfview/run.sh
+++ hdfview/run.sh
@@ -2,4 +2,5 @@ docker run --rm -it --privileged \
     -e DISPLAY=$DISPLAY  \
     -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0  \
     -v /mnt/labshare:/labshare \
+    -v /home/david/cornell/nowack_lab/labnotebook:/labnotebook \
     dhl88/hdfview
