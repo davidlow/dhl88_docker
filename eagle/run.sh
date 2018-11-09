518a13d3579cacd470c1f216e9e1344c9bc928c9
David Low: Manchot <dhl88@cornell.edu>
2018-11-09 13:15:23 -0500

    Eagle working!

--- eagle/run.sh
+++ eagle/run.sh
@@ -3,4 +3,6 @@ docker run --rm -it --privileged \
     -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0  \
     -v /mnt/labshare:/labshare \
     -v /mnt/labnotebook:/labnotebook \
+    -v /home/david/cornell/nowack_lab/eagle:/root/EAGLE \
+    -v /home/david/docker/dhl88/eagle/eaglelib/eagle-cad-libraries:/root/EAGLE/libraries/eagle-cad-libraries \
     dhl88/eagle
