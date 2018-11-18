f844f050f9d3fab75596709e9248a512b92f6b27
david <david@damddesk.home>
2018-11-18 16:36:39 -0500

    minor change to labpython run

--- labpython/run.sh
+++ labpython/run.sh
@@ -8,5 +8,5 @@ docker run \
     -v /mnt/labshare:/labshare \
     -v /home/david/cornell/labnotebook:/labnotebook \
     -v /home/david/cornell/nowack_lab/code:/code  \
-    --name labpython2 \
+    --name labpython \
     dhl88/labpython
