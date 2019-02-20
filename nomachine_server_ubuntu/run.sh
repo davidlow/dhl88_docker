61fa43830a72c28e25ec3ea0ce4936fa8376f4d1
David Low: Manchot <dhl88@cornell.edu>
2019-02-20 16:01:05 -0500

    ubuntu + nomachine + firefox,evince,okular
    A stable version of okular and evince are packaged with
    ubuntu 18.04 LTS.  It does the things that I want, like saving
    the annotations into the pdfs and having the annotation settings
    stored in your home folder in a reasonable place, not some random
    place

---
+++ nomachine_server_ubuntu/run.sh
@@ -0,0 +1,9 @@
+docker run \
+    -it \
+    --rm \
+    --privileged \
+    -p 4000:4000\
+    -v /home/david/cornell/nowack_lab/labnotebook/docker/home:/home  \
+    -v /home/david/library:/library \
+    --name nomachine_server_ubuntu \
+    dhl88/nomachine_server_ubuntu
