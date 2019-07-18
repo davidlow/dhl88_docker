cf542926bdc0e7a55288c9a57381a406cb2018f0
David Low: Manchot <dhl88@cornell.edu>
2019-07-18 11:21:16 -0400

    Java container with better mounts

--- java/run.sh
+++ java/run.sh
@@ -1,36 +1,36 @@
-# # - once in, run: 
-# jupyter lab --port=38888 --no-browser --allow-root --ip=0.0.0.0
-#
-# # - from the host machine, localhost:38888 is sufficient
-# # - from a docker container webbrowser, use the IP address of 
-# #   this docker container which you can find from the host by 
-# docker inspect anaconda0 | grep IP
-# # # All jupyter settings are stored in jupyterconfigs, which is on
-# # the host machine, so they are persistant
-
-DOCKERNAME=java_${USER}_0
+OFFSET=0
+DOCKERNAME=java_${USER}_${OFFSET}
 DOCKERCONTAINER=dhl88/java_${USER}
-#PORT=$((38888+$(id -u)))
 ECLIPSE_WORKSPACE=/home/david/cornell/nowack_lab/labnotebook/eclipse
 
+OPTS_DIR=run_options
+if [ -d "run_options/${HOSTNAME}/${USER}" ]; then
+    OPTS_DIR=run_options/${HOSTNAME}/${USER}
+fi
+MOUNT_OPTS=`grep -o '^[^#]*' ${OPTS_DIR}/mount_opts`
+DISPLAY_OPTS=`grep -o '^[^#]*' ${OPTS_DIR}/display_opts`
 
-# Change these
+NC='\\033[0m'
+RED='\\033[0;31m'
+GREEN='\\033[0;32m'
+NC_='\033[0m'
+RED_='\033[0;31m'
+GREEN_='\033[0;32m'
 
-    #-p $PORT:$PORT \
-    #-p 8787:8787 \
+echo -e "Starting ${GREEN_}${THISDOCKERNAME}${NC_} from ${RED_}${HOSTNAME}${NC_}"
+
+TMP=`sed "s/-v/${NC}Folder at ${HOSTNAME} ${RED}/g; s/:/${NC} is mounted in docker as ${GREEN}/g; s/$/${NC}/" ${OPTS_DIR}/mount_opts | grep Folder`
+echo -e "${TMP}"
+echo ""
+
+
 docker run \
     --rm  \
     -it \
     --privileged \
-    -e DISPLAY=$DISPLAY  \
-    -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0  \
     \
-    -v /mnt/labshare:/labshare \
-    -v /home/david/cornell/nowack_lab/labnotebook:/labnotebook \
-    -v /home/david/cornell/nowack_lab/code:/code  \
-    -v /home/david/library:/library\
-    -v $ECLIPSE_WORKSPACE:/home/${USER}/eclipse-workspace \
+    ${MOUNT_OPTS} \
+    ${DISPLAY_OPTS} \
     \
     --name $DOCKERNAME \
     $DOCKERCONTAINER
-
