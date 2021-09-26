9e08e7ea63e7f1eb6f098457403c3bfd69f931bf
David <david@david.com>
2021-09-26 02:38:28 -0400

    firefox_sid2 for merge

--- firefox_sid2/run.sh
+++ firefox_sid2/run.sh
@@ -4,13 +4,14 @@ docker run \
     --privileged \
     -e DISPLAY=$DISPLAY  \
     -v /home/david/Downloads:/Downloads \
+    -v /home/david/cornell:/cornell \
     -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0:ro  \
     -v /run/user/$(id -u)/pulse:/run/pulse:ro \
     -v /home/david/library:/library \
     -v /home/david/cornell/nowack_lab/labnotebook:/labnotebook \
     --shm-size 2g \
     --name firefox-sid2 \
-    dhl88/firefox-sid2
+    dhl88/firefox-sid2:20210618
 
 #    -v /dev/snd:/dev/snd \ # for alsa audio
 #    --device /dev/snd \
